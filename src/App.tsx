d5d53fc3b39e888bb37a8853d7988f8ad0fe0c76
Vedran Stankovic <contact@vedrans.me>
2024-05-19 21:28:55 +0100

    get users from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {  useEffect, useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
+
+const API_USERS_URL = "https://randomuser.me/api/?results=20";
+
+interface UserName {
+  first: string;
+}
+interface User {
+  name: UserName;
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [users, setUsers] = useState<User[]>([])
+
+  useEffect(() =>{
+    axios
+    .get(API_USERS_URL)
+    .then(r => {
+      setUsers(r.data.results)
+    })
+    .catch(e => console.error(e))
+
+  }, [])
 
   return (
     <>
+      <h1>Users</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {users.map((u, i) => (
+          <div key={i}>{u.name.first}</div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
