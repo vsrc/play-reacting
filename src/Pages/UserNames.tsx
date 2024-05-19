accdbf83eb1f5a7bdaaa138dc4d3aade541c2c2c
Vedran Stankovic <contact@vedrans.me>
2024-05-19 21:54:14 +0100

    link to homepage

--- src/Pages/UserNames.tsx
+++ src/Pages/UserNames.tsx
@@ -1,5 +1,6 @@
 import {useEffect, useState} from "react";
 import axios from 'axios'
+import { Link } from "react-router-dom";
 
 const API_USERS_URL = "https://randomuser.me/api/?results=20";
 
@@ -25,6 +26,7 @@ const UserNamesPage = () => {
 
   return (
     <>
+      <Link to="/">Homepage</Link>
       <h1>Users</h1>
       <div>
         {users.map((u, i) => (
