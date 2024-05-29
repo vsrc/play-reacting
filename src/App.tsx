2df31a997127201ecf81d1c7aebd48e09902c5d8
V <contact@vedrans.me>
2024-05-29 11:03:27 +0100

    setup react query provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,32 @@
 // import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import {QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import UserNamesPage from './Pages/UserNames'
 import WelcomePage from './Pages/Welcome';
 import Counter from './Pages/Counter';
 
 function App() {
 
+  const queryClient = new QueryClient();
 
   return (
     <>
       {/* <UserNamesPage /> */}
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Navigate to="/home" />} />
-          <Route path='/home' element={<WelcomePage />} />
-          <Route path='/counter' element={<Counter />} />
-          <Route path='/user' >
-            <Route path='names' element={<UserNamesPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Navigate to="/home" />} />
+            <Route path='/home' element={<WelcomePage />} />
+            <Route path='/counter' element={<Counter />} />
+            <Route path='/user' >
+              <Route path='names' element={<UserNamesPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   )
 }
